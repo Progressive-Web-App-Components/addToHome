6235d52b35c66b3dbb3389365dfb0d5d405bfa0c
Jagat Jeevan Sahoo <jagatsah@thoughtworks.com>
2018-02-16 11:38:42 +0530

    add to home

---
+++ sw.js
@@ -0,0 +1,28 @@
+self.addEventListener('install', function(event) {
+  console.log('SW installed ', version , ' -> ', new Date().toLocaleString());
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(version)
+    .then(function(cache) {
+      return cache.addAll([
+        'addToHome/index.html'
+      ]);
+    }));
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(res) {
+      if(res) {
+        return res;
+      }
+
+      if(!navigator.onLine) {
+        return caches.match(new Request('addToHome/index.html'));
+      }
+      // The below line is buggy. The fetch header needs to be cleared after a request is done.
+      //
+      return fetch(event.request);
+    }));
+});
